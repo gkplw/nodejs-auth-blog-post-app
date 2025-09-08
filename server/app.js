3fb2c8c089c226adcccd290b259704bf64df39ea
Ponlawat <p.sangtub@gmail.com>
2025-09-08 16:11:58 +0700

    feat: implement authentication

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import postRouter from "./apps/posts.js";
+import authRouter from "./apps/auth.js";
 import { client } from "./utils/db.js";
+
 
 async function init() {
   const app = express();
@@ -12,6 +14,8 @@ async function init() {
 
   app.use(cors());
   app.use(bodyParser.json());
+
+  app.use("/auth", authRouter);
   app.use("/posts", postRouter);
 
   app.get("/", (req, res) => {
